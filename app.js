b417ef95d74a7bf0d887c57d9de926694d61e7c0
Brandon Ryall-Ortiz <brandon_ryall-ortiz@comcast.com>
2020-10-06 15:31:22 -0400

    Minor fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ws = require('ws');
 
 const app = express();
-const port = $PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
